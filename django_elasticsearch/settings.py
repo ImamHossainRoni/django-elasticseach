7de3d3e92ca610d5d8c54fd7edb6a850ff5036cf
Imam Hossain Roni <ironi@iquantile.com>
2022-04-11 15:22:01 +0600

    elasticsearch document added

--- django_elasticsearch/settings.py
+++ django_elasticsearch/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_elasticsearch_dsl',
     'blog',
     'elastic_search',
     'rest_framework',
